e570af6d7d9a8669a25a496cdc57a2024ef19477
mikusiowatyy <122600245+mikusiowatyy@users.noreply.github.com>
2023-04-05 10:22:50 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Elo, jestem Mikołaj z Bydgoszczy lecze się w Choroszczy");
+        Console.WriteLine("Jak masz na imie");
+        string imie = Console.ReadLine(); 
+        Console.WriteLine("Cześć " + imie);
+        Console.WriteLine("on mi drink daje i robi ze mną jakieś dziwne akcje, a ile ty masz lat?");
+        string wiek = Console.ReadLine();
+        Console.WriteLine("xddddd " + wiek );
+        Console.WriteLine("Nazywam sie Kononowicz Krzysztof");
+        string leczenie = Console.ReadLine();
+        Console.WriteLine("Do Choroszczy na badania " + leczenie);
+        Console.ReadLine();
+        char znak = 'a';
+        int kod = sizeof(int);
+        Console
+        Console.WriteLine(znak);
+        int x = sizeof(char);
+        Console.WriteLine(x);
+        Console.ReadKey();
+       
+    }
+}
